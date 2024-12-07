ae2cc1fc323a307e70fa3764e6805569b4a45432
theselfcaresanctuary <warda.mughal19@gmail.com>
2024-12-07 06:04:19 +0500

    dd

--- infinite_bookshelf/ui/components/__init__.py
+++ infinite_bookshelf/ui/components/__init__.py
@@ -1,7 +1,15 @@
+"""UI Components"""
+
 from .basic_form import render_groq_form
 from .advanced_form import render_advanced_groq_form
 from .together_form import render_together_form
 from .statistics import display_statistics
 from .download import render_download_buttons
 
-__all__ = ["render_groq_form", "render_advanced_groq_form", "render_together_form", "display_statistics", "render_download_buttons"]
+__all__ = [
+    "render_groq_form",
+    "render_advanced_groq_form",
+    "render_together_form",
+    "display_statistics",
+    "render_download_buttons",
+]
